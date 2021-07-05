92c270c144f1b74ebd4bab710fc76714aa02aca4
Robersonsenna <86784090+Robersonsenna@users.noreply.github.com>
2021-07-05 20:52:29 -0300

    Projeto iniciado
    O projeto já foi iniciado

---
+++ src/sistemadegestão/Departamento.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemadegestão;
+
+/**
+ *
+ * @author rober
+ */
+public class Departamento {
+    private int codigo;
+    private string nome;
+    
+    public departamento () {
+           
+    
+}
+    
+public departamento (int codigo, string nome) { 
+    this.codigo = codigo;
+    this.nome = nome;
+}
---
+++ src/sistemadegestão/Funcionario.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemadegestão;
+
+/**
+ *
+ * @author rober
+ */
+public class Funcionario {
+    private int matricula; 
+    private string nome;
+    private departamento dep;
+    
+    public funcionario() {
+    
+}
+    
+public funcionario (int matricula, string nome) {
+    this.matricula = matricula;
+    this.nome = nome
+}
